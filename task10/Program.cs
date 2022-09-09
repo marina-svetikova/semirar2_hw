2050efb754becbdafe7ceadf330456e7fc9527bd
Svetikova Marina <m-golovleva@yandex.ru>
2022-09-09 21:57:23 +0500

    внесла изменения task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначне число");
+int number = Convert.ToInt32 (Console.ReadLine());
+int count1 = number / 10;
+int count2 = count1 % 10;
+Console.WriteLine (count2);
+
